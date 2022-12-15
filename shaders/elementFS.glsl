25aa9df2e389924f8fb59af34123936121732a76
gxrj <gsouzafone@gmail.com>
2022-12-14 22:27:07 -0300

    feat: set pixel colors based on its cartesian position

--- shaders/elementFS.glsl
+++ shaders/elementFS.glsl
@@ -3,5 +3,10 @@
 out vec4 color;
 
 void main( ) { 
-    color = vec4( 0.0, 1.0, 0.0, 1.0 );
+    if( gl_FragCoord.x < 365 )
+        color = vec4( 1.0, 0.0, 0.0, 1.0 );
+    else if( gl_FragCoord.x < 428 )
+        color = vec4( 0.0, 1.0, 0.0, 1.0 );
+    else
+        color = vec4( 0.0, 0.0, 1.0, 1.0 );
 }
