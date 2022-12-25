15648058e24eea5a77d8fab2434ecbeb43564fc3
gxrj <gsouzafone@gmail.com>
2022-12-25 00:36:22 -0300

    feat: set pixel colors based on its cartesian position variation

--- shaders/elementFS.glsl
+++ shaders/elementFS.glsl
@@ -1,12 +1,30 @@
 #version 460
 
+uniform float offset;
 out vec4 color;
 
+void drawOneColor();
+void drawMultipleColors();
+float getColorDelimiter( float );
+
 void main( ) { 
-    if( gl_FragCoord.x < 365 )
+   //drawOneColor();
+   drawMultipleColors(); 
+}
+
+void drawOneColor() {
+    color = vec4( 0.0, 1.0, 0.0, 1.0 );
+}
+
+void drawMultipleColors() {
+    if( gl_FragCoord.x < getColorDelimiter( offset ) )
         color = vec4( 1.0, 0.0, 0.0, 1.0 );
-    else if( gl_FragCoord.x < 428 )
+    else if( gl_FragCoord.x < getColorDemlimiter( offset ) + 50 )
         color = vec4( 0.0, 1.0, 0.0, 1.0 );
     else
         color = vec4( 0.0, 0.0, 1.0, 1.0 );
+}
+
+float getColorDelimiter( float offset ) {
+    return offset*400 + 370;
 }
